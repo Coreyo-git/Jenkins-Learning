e749b21e63609b93c50f9f048a23b2c9b42ffff2
coreyo-git <corey.oconnor@live.com.au>
2022-05-07 10:16:22 +1000

    Added smee webhook forwarding

---
+++ smee-webhook-Forwarding/smee_service.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# uses smee-client npm package, a webhook payload delivery service
+# Allows for webhook payloads without having to forward ports
+# Ran locally to forward github webhook payloads to Jenkins Client
+
+# Configure a github weekhook that points to the hosted endpoint/url
+smee --url https://smee.io/<ENDPOINT> --path /github/webhook/ --port <PORT>
