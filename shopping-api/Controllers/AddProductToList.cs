6631e796e4aa0e53ef0fc201b56c411eb771a6b6
coreyo-git <corey.oconnor@live.com.au>
2022-05-26 13:25:01 +1000

    Updated backend API's and tested them

--- shopping-api/Controllers/AddProductToList.cs
+++ shopping-api/Controllers/AddProductToList.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using shopping_api.Data;
+using shopping_api.Models;
 
 namespace shopping_api.Controllers;
 
@@ -6,5 +8,22 @@ namespace shopping_api.Controllers;
 [Route("[controller]")]
 public class AddProductToList : ControllerBase
 {
-    
+  private DatabaseContext _db;
+
+  public AddProductToList(DatabaseContext db)
+  {
+    _db = db;
+  }
+
+  [HttpPost]
+  [Route("/api/add_product/{product}")]
+  public IActionResult AddProduct(ProductList product)
+  {
+    if (product != null)
+    {
+      _db.product_list.Add(product);
+
+    }
+    return Ok();
+  }
 }
--- shopping-api/Controllers/GetShoppingList.cs
+++ shopping-api/Controllers/GetShoppingList.cs
@@ -5,34 +5,34 @@ using shopping_api.Models;
 namespace shopping_api.Controllers;
 
 [ApiController]
-public class GetProducts : ControllerBase
+public class GetShoppingList : ControllerBase
 {
 
   private DatabaseContext _db;
 
-  public GetProducts(DatabaseContext db)
+  public GetShoppingList(DatabaseContext db)
   {
     _db = db;
   }
 
   // Gets all the products in order from postgresql 
   [HttpGet]
-  [Route("/api/get_products")]
-  public IActionResult GetAllProducts()
+  [Route("/api/get_shopping_list")]
+  public IActionResult GetList()
   {
     // If the quote table is empty return 404
-    if (!_db.products.Any())
+    if (!_db.product_list.Any())
     {
       return NotFound("No Products in Table");
     }
     // Creates an enumerable list of products from the database
     // Order by ID
 
-    IEnumerable<Product> products = _db.products
+    IEnumerable<ProductList> product_list = _db.product_list
         .OrderBy(x => x.id)
         .Take(20);
 
-    return Ok(products);
+    return Ok(product_list);
   }
 
 }
