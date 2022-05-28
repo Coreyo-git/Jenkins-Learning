568fffd28262b369b00800700ae5f261ed75fbf1
coreyo-git <corey.oconnor@live.com.au>
2022-05-28 13:41:12 +1000

    Updated db init

--- database/init_db.sh
+++ database/init_db.sh
@@ -2,12 +2,5 @@
 
 # Imports the seed data on gen
 
-#!/bin/bash
-
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE USER jenkinsuser;
-    CREATE DATABASE shopping_db;
-    GRANT ALL PRIVILEGES ON DATABASE shopping_db TO jenkinsuser;
-EOSQL
-
-psql -f /db-dumps/db.dump.sql jenkinsuser
+su postgres
+psql -f /db-dumps/db.dump.sql
